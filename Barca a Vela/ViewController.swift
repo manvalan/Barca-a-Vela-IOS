829ca64030b5fdc3eaa7faa1296a58068c295379
Michele Bigi <michelebigi@macbook-air-di-michele.local>
2017-05-10 11:26:29 +0200

    Initial Commit

---
+++ Barca a Vela/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Barca a Vela
+//
+//  Created by Michele Bigi on 10/05/17.
+//  Copyright © 2017 Michele Bigi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
